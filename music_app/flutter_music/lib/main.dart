a757a06bc47a7e89f9af4c62353f91b4cb416a27
Nurali <izetkhannurali@gmail.com>
2024-05-10 20:51:43 +0500

    first commit

---
+++ music_app/flutter_music/lib/main.dart
@@ -0,0 +1,530 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+// Models
+class Song {
+  final String songName;
+  final String artistName;
+  final String albumArtImagePath;
+  final String audioPath;
+
+  Song({
+    required this.songName,
+    required this.artistName,
+    required this.albumArtImagePath,
+    required this.audioPath,
+  });
+}
+
+class PlaylistProvider extends ChangeNotifier {
+  final List<Song> _playlist = [
+    Song(
+      songName: "Fur Elise",
+      artistName: "Bethoven",
+      albumArtImagePath: "assets/images/bethovn.jpg",
+      audioPath: "audio/Beethoven-Fur Elise.mp3",
+    ),
+    Song(
+      songName: "Symphony 40",
+      artistName: "Mozart",
+      albumArtImagePath: "assets/images/mozart.jpeg",
+      audioPath: "audio/mocart-simfonija-40.mp3",
+    ),
+    Song(
+      songName: "River Flows in You",
+      artistName: "Yiruma",
+      albumArtImagePath: "assets/images/yiruma.jpeg",
+      audioPath: "audio/RiverFlowsInYou.mp3",
+    ),
+  ];
+
+  int? _currentSongIndex;
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+  bool _isPlaying = false;
+
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  void play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    await _audioPlayer.stop();
+    await _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pause() async {
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  void resume() async {
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pauseOrResume() async {
+    if (_isPlaying) {
+      pause();
+    } else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+  void seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  void playNextSong() {
+    if (_currentSongIndex != null) {
+      if (_currentSongIndex! < _playlist.length - 1) {
+        currentSongIndex = _currentSongIndex! + 1;
+      } else {
+        currentSongIndex = 0;
+      }
+    }
+  }
+
+  void playPreviousSong() async {
+    if (_currentDuration.inSeconds > 2) {
+      seek(Duration.zero);
+    } else {
+      if (_currentSongIndex! > 0) {
+        currentSongIndex = _currentSongIndex! - 1;
+      } else {
+        currentSongIndex = _playlist.length - 1;
+      }
+    }
+  }
+
+  void listenToDuration() {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPositionChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      playNextSong();
+    });
+  }
+
+  List<Song> get playlist => _playlist;
+  int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
+
+  set currentSongIndex(int? newIndex) {
+    _currentSongIndex = newIndex;
+
+    if (newIndex != null) {
+      play();
+    }
+
+    notifyListeners();
+  }
+}
+
+// Themes
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    inversePrimary: Colors.grey.shade900,
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Color.fromARGB(255, 51, 51, 51),
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
+
+// Components
+class NeuBox extends StatelessWidget {
+  final Widget? child;
+
+  const NeuBox({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<ThemeProvider>(context).isDarkMode;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: isDarkMode ? Colors.black : Colors.grey.shade500,
+            blurRadius: 15,
+            offset: const Offset(4, 4),
+          ),
+          BoxShadow(
+            color: isDarkMode ? Colors.grey.shade800 : Colors.white,
+            blurRadius: 15,
+            offset: const Offset(-4, -4),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(12),
+      child: child,
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.music_note,
+                size: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25),
+            child: ListTile(
+              title: const Text("H O M E"),
+              leading: const Icon(Icons.home),
+              onTap: () => Navigator.pop(context),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 0),
+            child: ListTile(
+              title: const Text("S E T T I N G S"),
+              leading: const Icon(Icons.settings),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SettingsPage(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Pages
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final dynamic playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+
+  void goToSong(int songIndex) {
+    playlistProvider.currentSongIndex = songIndex;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SongPage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(title: const Text("P L A Y L I S T")),
+      drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          final List<Song> playlist = value.playlist;
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              final Song song = playlist[index];
+              return ListTile(
+                title: Text(song.songName),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.albumArtImagePath),
+                onTap: () => goToSong(index),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SongPage extends StatelessWidget {
+  const SongPage({Key? key}) : super(key: key);
+
+  String formatTime(Duration duration) {
+    String twoDigitSeconds =
+        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    String formattedTime = "${duration.inMinutes}:$twoDigitSeconds";
+    return formattedTime;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PlaylistProvider>(
+      builder: (context, value, child) {
+        final playlist = value.playlist;
+        final currentSong = playlist[value.currentSongIndex ?? 0];
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: const Icon(Icons.arrow_back),
+                      ),
+                      const Text("P L A Y L I S T "),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.menu),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  NeuBox(
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(currentSong.albumArtImagePath),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    currentSong.songName,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text(currentSong.artistName)
+                                ],
+                              ),
+                              const Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(formatTime(value.currentDuration)),
+                            const Icon(Icons.shuffle),
+                            const Icon(Icons.repeat),
+                            Text(formatTime(value.totalDuration)),
+                          ],
+                        ),
+                      ),
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 0),
+                        ),
+                        child: Slider(
+                          min: 0,
+                          max: value.totalDuration.inSeconds.toDouble(),
+                          value: value.currentDuration.inSeconds.toDouble(),
+                          activeColor: Colors.green,
+                          onChanged: (double double) {},
+                          onChangeEnd: (double double) {
+                            value.seek(Duration(seconds: double.toInt()));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: value.playPreviousSong,
+                          child: const NeuBox(
+                            child: Icon(Icons.skip_previous),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: value.pauseOrResume,
+                          child: NeuBox(
+                            child: Icon(value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: value.playNextSong,
+                          child: const NeuBox(
+                            child: Icon(Icons.skip_next),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: const Text("S E T T I N G S"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Dark Mode",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            CupertinoSwitch(
+              value:
+                  Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+              onChanged: (value) =>
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .toggleTheme(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PlaylistProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const HomePage(),
+    );
+  }
+}
